d045e7b172b5aee8a323b83f79ca1ae06c584c4a
carlosyanceara <carlosyanceara@gmail.com>
2025-04-19 13:20:43 +0000

    feat: implemeted provider

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tech_taste_alura/data/restaurant_data.dart';
+import 'package:tech_taste_alura/ui/_core/app_theme.dart';
+import 'package:tech_taste_alura/ui/splash/splash_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  RestaurantData restaurantData = RestaurantData();
+  await restaurantData.getRestaurants();
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) {
+            return restaurantData;
+          },
+        ),
+      ],
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.appTheme,
+      home: SplashScreen(),
+    );
+  }
+}
